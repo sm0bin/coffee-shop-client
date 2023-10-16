6ca63d6d2c10fdc907102a1aa03332536c9553cf
GhostMobin <s1810677110@ru.ac.bd>
2023-10-16 14:49:12 +0600

    sweet alert

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,15 @@
 const Home = () => {
     return (
         <div>
-            <h1 className="text-center font-bold text-5xl">This is home</h1>
+            <h4 className="text-center text-xl">--- Sip & Savor ---</h4>
+            <h1 className="font-rancho text-center font-bold text-6xl mt-2 mb-4">Our Popular Products</h1>
+            <button className="btn block mb-12 mx-auto bg-[#D2B48C] hover:bg-[#cb9a5b] font-rancho normal-case text-2xl  text-[#331A15] border-2 hover:border-[#331A15]">Add Coffee</button>
+
+            <div className="grid grid-cols-1 md:grid-cols-2">
+
+            </div>
         </div>
+
     );
 };
 
