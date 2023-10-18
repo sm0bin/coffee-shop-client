0104138ab2dc05c3ec71c2f8ceb5b9b4ba2b7868
GhostMobin <s1810677110@ru.ac.bd>
2023-10-18 18:19:14 +0600

    env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBDOChBrjeWKRjIQkL9w9qqHx_NPgrSC-4",
-    authDomain: "coffee-shop-b3b46.firebaseapp.com",
-    projectId: "coffee-shop-b3b46",
-    storageBucket: "coffee-shop-b3b46.appspot.com",
-    messagingSenderId: "699966578887",
-    appId: "1:699966578887:web:b9e55264de13d238495ee2"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
